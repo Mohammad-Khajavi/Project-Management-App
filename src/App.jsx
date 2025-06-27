b129a2600ec21c4669e009bd1b9f1414a002da76
mohammad <151404726+Mohammad-Khajavi@users.noreply.github.com>
2025-06-26 20:59:32 -0400

    Feat: Added some Tailwindcss styling to the ProjectsSideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import AuthInputs from './components/AuthInputs.jsx';
-import Header from './components/Header.jsx';
+import AuthInputs from "./components/AuthInputs.jsx";
+import Header from "./components/Header.jsx";
+import ProjectSideBar from "./components/ProjectsSideBar.jsx";
 
 export default function App() {
   return (
     <>
-      <Header />
-      <main>
-        <AuthInputs />
+      <main className="h-screen my-8 ">
+        <ProjectSideBar />
       </main>
     </>
   );
