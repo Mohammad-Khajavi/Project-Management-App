964d8ce10c856ff378a828f37643ba4d140510c4
mohammad <151404726+Mohammad-Khajavi@users.noreply.github.com>
2025-06-27 00:09:46 -0400

    feat: Created a storyBook for a componenet for testing. Added new componenets such as Button and noProjectSelected.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import AuthInputs from "./components/AuthInputs.jsx";
 import Header from "./components/Header.jsx";
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectSideBar from "./components/ProjectsSideBar.jsx";
 
 export default function App() {
@@ -8,7 +9,7 @@ export default function App() {
     <>
       <main className="h-screen my-8 flex gap-8">
         <ProjectSideBar />
-        <NewProject />
+        <NoProjectSelected />
       </main>
     </>
   );
---
+++ src/components/NoProjectSelected.stories.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import NoProjectSeclected from "./NoProjectSelected.jsx";
+
+//Default export with elements
+export default {
+  title: "Components/NoProjectSeclected",
+  component: NoProjectSeclected,
+};
+
+export const Default = () => <NoProjectSeclected />;
