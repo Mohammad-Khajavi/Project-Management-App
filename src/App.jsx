84f92172d6adb09eaa6c67084e5f57473546b260
mohammad <151404726+Mohammad-Khajavi@users.noreply.github.com>
2025-06-28 19:36:43 -0400

    Added the Logic for navigation between componenets.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import AuthInputs from "./components/AuthInputs.jsx";
 import Header from "./components/Header.jsx";
 import NewProject from "./components/NewProject.jsx";
@@ -5,11 +6,32 @@ import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectSideBar from "./components/ProjectsSideBar.jsx";
 
 export default function App() {
+  const [projectsStates, setProjectsStates] = useState({
+    selectedProjectId: undefined,
+    projectArray: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsStates((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsStates.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsStates.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <ProjectSideBar />
-        <NoProjectSelected />
+        <ProjectSideBar onStartAddProject={handleStartAddProject} />
+        {content}
       </main>
     </>
   );
--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import noProjectImage from "../assets/no-projects.png";
 import Button from "./Button.jsx";
 
-export default function NoProjectSelected() {
+export default function NoProjectSelected({ onStartAddProject }) {
   return (
     <div className="mt-24 text-center w-2/3">
       <img
@@ -17,7 +17,7 @@ export default function NoProjectSelected() {
         Seclect A project or get Started with a new On
       </p>
       <p className="mt-8">
-        <Button>Create new Project</Button>
+        <Button onClick={onStartAddProject}>Create new Project</Button>
       </p>
     </div>
   );
